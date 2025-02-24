64b496f945889d3181a3006958d64a2ebe712f3f
yoonbeeseo <yoon.tec.info@gmail.com>
2025-02-24 12:55:55 +0900

    tsx init

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
 import { useEffect, useState } from "react"
 
+//Todo Person type 만들기 name: string, age: number
+
+//Todo Pet type 만들기 name: string; age: number; weight: number; desexed: boolean
+
 const App = () => {
-  // useState 와 ()사이에 <> 안에 타입을 지정해줌
-  const [a, setA] = useState<number>(0)
+  const [a, setA] = useState<string>("")
+  const [b, setB] = useState<number>(0)
 
-  const [s, setS] = useState<string>("0")
-  // const [s, setS] = useState<number>(0)
+  const [pet1, setPet1] = useState({}) //Todo: Pet 1 만들기
+  const [p1, setP1] = useState({}) //Todo: Person 1 만들기
 
-  const [b, setB] = useState<boolean>(false) // true
-  // const [b, setB] = useState<number>(0)
+  const [pets, setPets] = useState([]) // Pet을 담은 배열
+  const [people, setPeople] = useState([]) // Person 을 담은 배열
 
   useEffect(() => {
-    setA(12)
+    //Todo: pets에 pet1 추가하기
+    //Todo: people에 p1 추가하기
   }, [])
 
-  return <h1>App: {a}</h1>
+  return <h1>App</h1>
 }
 
 export default App
